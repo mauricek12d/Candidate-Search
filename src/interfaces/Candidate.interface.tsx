b3cb65a7da584647f047bd86148653d10b0ebf88
Maurice Zuniga <mauricek12d@gmail.com>
2024-12-09 14:59:31 -0500

    Add files via upload

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,12 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+export interface Candidate {
+    id: number; 
+    name: string; 
+    username: string; 
+    location: string; 
+    avatar: string; // The URL to the candidate's avatar image
+    email: string; // The candidate's email address
+    html_url: string; // The candidate's profile URL 
+    company: string; //  The candidate's company
+  }
+  
